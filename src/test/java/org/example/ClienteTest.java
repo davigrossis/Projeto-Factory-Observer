f96c7cecd5777fb62d002c5e3bf2b14c7cea17bf
Davi Grossi Soares de Souza <davi.grossi.souza@gmail.com>
2025-04-29 17:12:03 -0300

    utilizando todos os metodos no test: enviar, cancelar, preparar e aceitar

--- src/test/java/org/example/ClienteTest.java
+++ src/test/java/org/example/ClienteTest.java
@@ -6,13 +6,43 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ClienteTest {
 
     @Test
-    void deveNotificarCliente() {
+    void deveNotificarClienteAceitar() {
         Pedido pedido = new Pedido("Restaurante");
         Cliente cliente = new Cliente("Maria");
         pedido.addObserver(cliente);
         pedido.atualizarStatus("Pedido aceito pelo restaurante");
 
         assertEquals("Cliente Maria recebeu notificação: Restaurante atualizou o status do seu pedido: Pedido aceito pelo restaurante", cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void deveNotificarClienteCancelar() {
+        Pedido pedido = new Pedido("Farmacia");
+        Cliente cliente = new Cliente("Carlos");
+        pedido.addObserver(cliente);
+        pedido.atualizarStatus("Pedido de remedios cancelado pelo cliente");
+
+        assertEquals("Cliente Carlos recebeu notificação: Farmacia atualizou o status do seu pedido: Pedido de remedios cancelado pelo cliente", cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void deveNotificarClientePreparar() {
+        Pedido pedido = new Pedido("Mercado");
+        Cliente cliente = new Cliente("Joana");
+        pedido.addObserver(cliente);
+        pedido.atualizarStatus("Produtos estão sendo separados");
+
+        assertEquals("Cliente Joana recebeu notificação: Mercado atualizou o status do seu pedido: Produtos estão sendo separados", cliente.getUltimaNotificacao());
+    }
+
+    @Test
+    void deveNotificarClienteEnviar() {
+        Pedido pedido = new Pedido("Restaurante");
+        Cliente cliente = new Cliente("Bruno");
+        pedido.addObserver(cliente);
+        pedido.atualizarStatus("Comida saiu para entrega com o entregador");
+
+        assertEquals("Cliente Bruno recebeu notificação: Restaurante atualizou o status do seu pedido: Comida saiu para entrega com o entregador", cliente.getUltimaNotificacao());
     }
 
     @Test
